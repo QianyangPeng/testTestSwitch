ad474bc2b9ea304634aacaa255b9ef8d393c8c6a
Qianyang Peng <qp3@illinois.edu>
2019-03-29 15:36:04 -0500

    init

---
+++ qp3/src/test/java/muyou/App8Test.java
@@ -0,0 +1,36 @@
+package muyou;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.lang.System;
+
+/**
+ * Unit test for simple App.
+ */
+public class App8Test
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println(System.currentTimeMillis());
+        assertTrue( true );
+    }
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue( true );
+    }
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+    public void shouldAnswerWithTrue4()
+    {
+        assertTrue( true );
+    }
+}
